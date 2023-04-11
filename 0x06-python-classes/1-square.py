fbe2001c562c7546533e3937f286232ea4cdf00a
Macharia777 <gacheruarchibald@gmail.com>
2023-04-11 04:46:19 -0700

    Classes and Objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class Square declaration"""
+
+
+class Square:
+    """My square class"""
+
+    def __init__(self, size):
+        """My Square initialization"""
+        self.__size = size
+
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# 0-square.py by Archibald Macharia
+"""My square declaration"""
+
+
+class Square:
+    """Square class introduction"""
+
+    def __init__(self, size=0):
+        """Initializing my square class
+        Args:
+            size: size of my square
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
+
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """Retrieves my square size"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """
+        Calculate area of the square
+        Returns: The square of the size
+        """
+
+        return (self.__size ** 2)
